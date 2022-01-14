d1cc4cea7958c03a5c7138a71e62661b50c3b90f
DanNimara <danynimara@gmail.com>
2022-01-14 02:11:23 +0200

    pm, save

--- CyberPunk/Assets/Scripts/PlayerController.cs
+++ CyberPunk/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -261,4 +262,5 @@ public class PlayerController : MonoBehaviour
         bounceAmount = bounceForce;
         bounce = true;
     }
+
 }
